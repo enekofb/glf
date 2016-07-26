76923b2f263bbb578ed9757ac86ca5d8d0ef2cb9
Eneko Fernandez <eneko.fernandez@opencredo.com>
2016-07-26 13:25:42 +0100

    private and public subnet

--- terraform/src/main/resources/conf/etcd.tf
+++ terraform/src/main/resources/conf/etcd.tf
@@ -4,10 +4,6 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
-output "output" {
-  value = "${access_key}"
-}
-
 resource "aws_vpc" "eneko-vpc" {
   cidr_block = "10.20.0.0/16"
   tags = {
@@ -22,15 +18,33 @@ resource "aws_internet_gateway" "eneko-vpc-gw" {
   }
 }
 
-resource "aws_subnet" "eneko-subnet1" {
-  vpc_id = "${aws_vpc.eneko-vpc.id}"
-  cidr_block = "10.20.1.0/24"
-  availability_zone = "eu-west-1a"
+resource "aws_elb" "etcd" {
+  name = "etcd-elb"
+  subnets = [
+    "${aws_subnet.eneko-subnet1.id}"]
+  instances = [
+    "${aws_instance.etcd-ec2.id}"]
+  security_groups = [
+    "${aws_security_group.eneko-sg-all.id}"]
 
-  tags = {
-    Name = "eneko"
+  listener {
+    instance_port = 4001
+    instance_protocol = "http"
+    lb_port = 4001
+    lb_protocol = "http"
   }
 
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "http:4001/v2/keys/helloworld"
+    interval = 30
+  }
+
+  tags {
+    Name = "eneko"
+  }
 }
 
 resource "aws_security_group" "eneko-sg-all" {
@@ -60,6 +74,15 @@ resource "aws_security_group" "eneko-sg-all" {
 
 }
 
+resource "aws_subnet" "eneko-subnet1" {
+  vpc_id = "${aws_vpc.eneko-vpc.id}"
+  cidr_block = "10.20.1.0/24"
+  availability_zone = "eu-west-1a"
+  tags = {
+    Name = "eneko"
+  }
+
+}
 
 resource "aws_route_table" "eneko-vpc-routing-table" {
   vpc_id = "${aws_vpc.eneko-vpc.id}"
@@ -80,8 +103,47 @@ resource "aws_route_table_association" "eneko-vpc-routing-table-association1" {
   route_table_id = "${aws_route_table.eneko-vpc-routing-table.id}"
 }
 
-resource "aws_instance" "etcd-ec2" {
+resource "aws_subnet" "eneko-private-subnet1" {
+  vpc_id = "${aws_vpc.eneko-vpc.id}"
+  cidr_block = "10.20.10.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "eneko.private"
+  }
+
+}
+
+resource "aws_eip" "eneko-private-subnet1-gw-eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "eneko-private-subnet1-gw" {
   subnet_id = "${aws_subnet.eneko-subnet1.id}"
+  allocation_id = "${aws_eip.eneko-private-subnet1-gw-eip.id}"
+
+}
+
+resource "aws_route_table" "eneko-private-subnet1-route-table" {
+  vpc_id = "${aws_vpc.eneko-vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.eneko-private-subnet1-gw.id}"
+  }
+
+  tags {
+    Name = "eneko"
+  }
+}
+
+resource "aws_route_table_association" "eneko-private-subnet1-route-table-assoc" {
+  subnet_id = "${aws_subnet.eneko-private-subnet1.id}"
+  route_table_id = "${aws_route_table.eneko-private-subnet1-route-table.id}"
+}
+
+resource "aws_instance" "etcd-ec2" {
+  subnet_id = "${aws_subnet.eneko-private-subnet1.id}"
   vpc_security_group_ids = [
     "${aws_security_group.eneko-sg-all.id}"]
   ami = "ami-cbb5d5b8"
@@ -89,38 +151,6 @@ resource "aws_instance" "etcd-ec2" {
   key_name = "eneko-glf"
 
   tags = {
-    Name = "eneko"
-  }
-
-}
-
-//resource "aws_eip" "etcd-ec2-eip" {
-//  vpc = true
-//  instance = "${aws_instance.etcd-ec2.id}"
-//}
-
-resource "aws_elb" "etcd" {
-  name = "etcd-elb"
-  subnets = ["${aws_subnet.eneko-subnet1.id}"]
-  instances = ["${aws_instance.etcd-ec2.id}"]
-  security_groups = ["${aws_security_group.eneko-sg-all.id}"]
-
-  listener {
-    instance_port = 4001
-    instance_protocol = "http"
-    lb_port = 4001
-    lb_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "http:4001/v2/keys/helloworld"
-    interval = 30
-  }
-
-  tags {
     Name = "eneko"
   }
 }
